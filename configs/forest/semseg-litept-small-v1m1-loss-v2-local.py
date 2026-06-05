324ce01df6e888b6ee11d468e918fef23f5b5099
caozhou <cao.zhou.gd@gmail.com>
2026-06-05 22:13:17 +0800

    feat: loss-v2 validated (mIoU 0.5529->0.6092), postprocess, grid04 config ready

--- configs/forest/semseg-litept-small-v1m1-loss-v2-local.py
+++ configs/forest/semseg-litept-small-v1m1-loss-v2-local.py
@@ -5,9 +5,12 @@ save_path = "exp/forest/semseg-litept-small-v1m1-loss-v2"
 weight = "exp/forest/semseg-litept-small-v1m1/model/model_best.pth"
 resume = False
 
+use_gpu_transform = True
+
 batch_size = 1
-crop_point_max = 150000
+crop_point_max = 100000
 num_worker = 1
+empty_cache_per_epoch = True
 
 data = dict(
     block_xy=20,
